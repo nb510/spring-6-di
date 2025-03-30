785a24bf2e9d980b43984da62a4af610b799054a
nb510 <nikita.burak.dev@gmail.com>
2025-03-30 19:10:25 +0300

    add example of using Application context to get Bean

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -1,13 +1,21 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6DiApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+        System.out.println("Inside main");
+
+        MyController myController = (MyController) ctx.getBean("myController");
+        String result = myController.sayHello();
+        System.out.println(result);
     }
 
 }
