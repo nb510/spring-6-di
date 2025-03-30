c99430e11e721c496ed5c2b283f81b8d7376fd12
nburak <nburak@fnc-group.ru>
2025-03-30 20:32:38 +0300

    test injection without spring

---
+++ src/main/java/guru/springframework/spring6di/controller/SetterInjectedController.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring6di.controller;
+
+import guru.springframework.spring6di.service.GreetingService;
+
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
