5f8fd0b25acbe9e45821e685b91a0fd1fa8625a7
nb510 <nikita.burak.dev@gmail.com>
2025-03-30 19:20:19 +0300

    add tests

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controller.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowiredBean() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testBeanFromTheContext() {
+        MyController bean = applicationContext.getBean(MyController.class);
+        System.out.println(bean.sayHello());
+    }
 
     @Test
     void contextLoads() {
